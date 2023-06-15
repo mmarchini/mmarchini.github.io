483dacdaeef49ce094ea77063e0e4c7c00c5ebbc
Mary Marchini <oss@mmarchini.me>
2023-06-15 00:45:15 -0700

    a new beginning

---
+++ header.js
@@ -0,0 +1,38 @@
+const tmpl = document.createElement('template');
+tmpl.innerHTML = `
+  <div class=grid-container>
+    <div class="grid-100">
+      <h2 style="text-align: center; margin-bottom: 5px; margin-top: 15px;">mary's little big website</h2>
+    </div>
+  </div>
+
+  <div class=grid-container style="border-bottom: 1px dashed var(--primary-color); padding-bottom: 25px; margin-bottom: 15px;">
+    <div class="grid-100" style="text-align: center;">
+      <nav class="terminal-prompt">
+        <a href="/about.html">about</a>
+        <a href="/open-source.html">open source</a>
+        <a href="/conference-talks.html">conference talks</a>
+        <a href="/career.html">career</a>
+        <a href="https://dev.to/mmarchini">blog</a>
+        <a href="https://github.com/mmarchini">github</a>
+        <a href="https://www.linkedin.com/in/mary-marchini/">linkedin</a>
+      </nav>
+    </div>
+  </div>
+`;
+
+class MaryHeader extends HTMLElement {
+  constructor() {
+    // Always call super first in constructor
+    super();
+
+    // write element functionality in here
+  }
+
+  connectedCallback() {
+    // this.appendChild('<h1>foo</h1>');
+    this.appendChild(tmpl.content.cloneNode(true));
+  }
+}
+
+customElements.define("mary-header", MaryHeader);
